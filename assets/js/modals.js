7ba952e0211e7bbfbb0a33aee6fd05deadbc2137
jpugmire <jospugg@gmail.com>
2022-04-07 16:02:09 -0400

    events can now be added and removed with respective buttons and persist through reload

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -9,7 +9,9 @@ function renderAddEventModal() {
 
                     <p>Which Family?</p>
                     <button id="event_name1" class="modal-input1" >Johnson  </button>
-                    <button id="event_name2" class="modal-input2" > Smith </button> 
+                    <button id="event_name2" class="modal-input2" > Smith </button>
+                    <div>Title:</div>
+                    <input id="event_title" class="modal-input" type="text" placeholder="Title">
                     <br>
                     <p>Date:</p>
                     <input id="event_date" class="modal-input" type="text" placeholder="Date">
@@ -21,7 +23,7 @@ function renderAddEventModal() {
                     <input id="event_location" class="modal-input" type="text" placeholder="Location">
                         <br>
 
-                    <button id="create" class="">Create</button>
+                    <button id="create" onclick="saveSessionEvent();removeAddEventModal()">Create</button>
                     <button id="cancel" onclick="removeAddEventModal()">Cancel</button>
                 </div>
             </container>
@@ -38,6 +40,7 @@ function removeAddEventModal() {
 
 function renderEventInfoModal() {
     var element = document.getElementById('eventInfoModalContainer');
+    var target = this.id.match(/^\d+|\d+\b|\d+(?=\w)/g);
     element.innerHTML = `
         <section id="overlay_2">
             <container id="event_modal_container" class="open-info-modal">
@@ -57,11 +60,15 @@ function renderEventInfoModal() {
 
                 <button id="edit">Edit</button>
                 <button id="cancel-2" onclick="removeEventInfoModal()">Cancel</button>
+                <button id="delete-event" onclick="removeEventButton()">Delete</button>
 
             </div>
             </container>
         </section>
     `;
+    element.eventTarget = target;
+    //identify which event this is
+
 }
 
 function removeEventInfoModal() {
